a354561f2777b0de629d1a9aad7f77ed7294e5c4
horaceo1b <horaceken0@gmail.com>
2025-06-08 17:13:24 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+// 1. Declare companyName using const and set it to "Scuber"
+const companyName = 'Scuber';
+
+// 2. Declare mostProfitableNeighborhood using let and assign a value
+let mostProfitableNeighborhood = 'Chelsea';
+
+// 3. Declare companyCeo using let and assign a value
+let companyCeo = 'Susan Smith';
+
